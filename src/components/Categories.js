c00da6602952d80964188943f63144304b8e5279
Julie Mahe <j-mahe@hotmail.fr>
2021-02-08 19:08:23 +0100

    first day gone

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,18 +4,21 @@ const Categories = ({ data, isLoading }) => {
   const meals = data.meals;
   console.log(meals); // je récupère bien mon tableau dans mon console.log
   return (
-    <div>
-      <h3 className="categories">{data.name}</h3>
-      {isLoading ? (
-        <span>En cours de chargement... </span>
-      ) : (
-        meals.map((elem) => {
-          // je souhaite mapper le tableau meals de mon data
-          return <div key={elem.id}>{elem}</div>;
-          // <Meals data={data}/>
-        })
-      )}
-    </div>
+    <>
+      <div className="categorie">
+        <h3>{data.name}</h3>
+        <div className="meals">
+          {isLoading ? (
+            <span>En cours de chargement... </span>
+          ) : (
+            meals.map((elem) => {
+              // je souhaite mapper le tableau meals de mon data
+              return <Meals key={elem.id} data={elem} />;
+            })
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 export default Categories;
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,20 @@
 const Meals = ({ data }) => {
-  return <div>{data}</div>;
+  const popular = data.popular;
+  const description = data.description;
+
+  return (
+    <div className="meal">
+      <div className="leftPart">
+        <h4>{data.title}</h4>
+        {}
+        <p>{description.substring(0, 60)}</p>
+        <span>{data.price} € </span>
+        {popular && <span class="popular"> Populaire</span>}
+      </div>
+      <div className="rightPart">
+        <img src={data.picture} alt="" />
+      </div>
+    </div>
+  );
 };
 export default Meals;

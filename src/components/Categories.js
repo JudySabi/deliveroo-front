c4861bcf05670d696eae8589e79be7cd47ae2a1f
Julie Mahe <j-mahe@hotmail.fr>
2021-02-08 19:38:07 +0100

    free

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,19 +5,21 @@ const Categories = ({ data, isLoading }) => {
   console.log(meals); // je récupère bien mon tableau dans mon console.log
   return (
     <>
-      <div className="categorie">
-        <h3>{data.name}</h3>
-        <div className="meals">
-          {isLoading ? (
-            <span>En cours de chargement... </span>
-          ) : (
-            meals.map((elem) => {
-              // je souhaite mapper le tableau meals de mon data
-              return <Meals key={elem.id} data={elem} />;
-            })
-          )}
+      {meals && (
+        <div className="categorie">
+          <h3>{data.name}</h3>
+          <div className="meals">
+            {isLoading ? (
+              <span>En cours de chargement... </span>
+            ) : (
+              meals.map((elem) => {
+                // je souhaite mapper le tableau meals de mon data
+                return <Meals key={elem.id} data={elem} />;
+              })
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,18 +1,18 @@
 const Meals = ({ data }) => {
   const popular = data.popular;
   const description = data.description;
+  const picture = data.picture;
 
   return (
     <div className="meal">
       <div className="leftPart">
         <h4>{data.title}</h4>
-        {}
         <p>{description.substring(0, 60)}</p>
         <span>{data.price} € </span>
         {popular && <span class="popular"> Populaire</span>}
       </div>
-      <div className="rightPart">
-        <img src={data.picture} alt="" />
+      <div className="rightPart" style={{ marginRight: !picture && "130px" }}>
+        {data.picture && <img src={picture} alt="" />}
       </div>
     </div>
   );
